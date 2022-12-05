13007330b2481396698889e4d11216a801fc29ec
Dominique Foley <dfoleydev@gmail.com>
2022-12-05 11:15:03 -0500

    Style changes

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-//Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch() {
-  const choice = document.querySelector('input').value
-  let c = choice.charAt(0).toUpperCase() + choice.slice(1)
-  //console.log(c)
-  const url = `https://ddragon.leagueoflegends.com/cdn/12.7.1/data/en_US/champion/${c}.json`
-  document.getElementById('none').style.display = 'none'
-  document.getElementById('none2').style.display = 'none'
+	const choice = document.querySelector('input').value
+	let c = choice.charAt(0).toUpperCase() + choice.slice(1)
+	//console.log(c)
+	const url = `https://ddragon.leagueoflegends.com/cdn/12.7.1/data/en_US/champion/${c}.json`
+	document.getElementById('none').style.display = 'none'
+	document.getElementById('none2').style.display = 'none'
 
-  fetch(url)
-    .then((res) => res.json()) // parse response as JSON
-    .then((data) => {
-      //console.log(data)
-      //console.log(data.data[c].lore)
-      //data.data.Ashe.image.full to get image
-      document.getElementById('name').innerText = [c]
-      document.getElementById('lore').innerText = data.data[c].lore
-      document.querySelector(
-        'img'
-      ).src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${c}_0.jpg`
-    })
-    .catch((err) => {
-      console.log(`error ${err}`)
-    })
+	fetch(url)
+		.then((res) => res.json()) // parse response as JSON
+		.then((data) => {
+			console.log(data)
+			//console.log(data.data[c].lore)
+			//data.data.Ashe.image.full to get image
+			document.getElementById('name').innerText = [c]
+			document.getElementById('lore').innerText = data.data[c].lore
+			document.getElementById('title').innerText =
+				data.data[c].title.slice(4)
+			document.querySelector(
+				'img'
+			).src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${c}_0.jpg`
+		})
+		.catch((err) => {
+			console.log(`error ${err}`)
+		})
 }
-
