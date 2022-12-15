5cb31059de0fcbe8bf0567b5c285a770f9879af1
Dominique Foley <dfoleydev@gmail.com>
2022-12-14 22:14:38 -0500

    Refactoring design and functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch() {
+<<<<<<< Updated upstream
 	const choice = document.querySelector('input').value
 	let c = choice.charAt(0).toUpperCase() + choice.slice(1)
 	//console.log(c)
@@ -25,4 +26,28 @@ function getFetch() {
 		.catch((err) => {
 			console.log(`error ${err}`)
 		})
+=======
+    const choice = document.querySelector('input').value
+    let c = choice.charAt(0).toUpperCase() + choice.slice(1)
+    //console.log(c)
+    const url = `https://ddragon.leagueoflegends.com/cdn/12.7.1/data/en_US/champion/${c}.json`
+    document.getElementById('none').style.display = 'none'
+    document.getElementById('none2').style.display = 'none'
+
+    fetch(url)
+        .then((res) => res.json()) // parse response as JSON
+        .then((data) => {
+            console.log(data)
+            //console.log(data.data[c].lore)
+            //data.data.Ashe.image.full to get image
+            document.getElementById('name').innerText = [c]
+            document.getElementById('lore').innerText = data.data[c].lore
+            document.querySelector(
+                'img',
+            ).src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${c}_0.jpg`
+        })
+        .catch((err) => {
+            console.log(`error ${err}`)
+        })
+>>>>>>> Stashed changes
 }
